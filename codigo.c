df7f05378432643799ec62c0650aec9fe6e11555
MiniMestri <fonsild97@hotmail.com>
2023-11-04 17:56:07 +0100

    Desarrollo del codigo
    He desarrollado las cuatro funciones principales "crear:archivo", "insertar","buscar" y "eliminar".

---
+++ codigo.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void insertar(char *bbdd,char *equipo,char *lista,char *contenido){
+   
+    FILE *archivo;
+    
+    char *ruta[200];
+    
+    strcpy(ruta,"db/");
+    strcat(ruta,bbdd);
+    strcat(ruta,"/");
+    strcat(ruta,equipo);
+    strcat(ruta,"/");
+    strcat(ruta,lista);
+    strcat(ruta,".json");
+    
+    archivo = fopen(ruta,"a");
+    fputs(strcat(contenido,"\n"),archivo);
+    fclose(archivo);
+    
+}
+void buscar(char *bbdd,char *equipo,char *lista){    
+    FILE *archivo;
+    
+    int incremento=1;
+    
+    char *ruta[200];
+    char linea[1024];
+    
+    strcpy(ruta,"db/");
+    strcat(ruta,bbdd);
+    strcat(ruta,"/");
+    strcat(ruta,equipo);
+    strcat(ruta,"/");
+    strcat(ruta,lista);
+    strcat(ruta,".json");
+    
+    archivo=fopen(ruta,"r");
+    
+    while(fgets(linea,sizeof(linea),archivo) != NULL){
+        printf("Linea %d : %s",incremento,linea);
+        incremento++;
+    }
+    fclose(archivo);
+    
+}
+void eliminar(char *bbdd, char *equipo, char *lista){
+    
+    char *ruta[200];
+    
+    strcpy(ruta,"db/");
+    strcat(ruta,bbdd);
+    strcat(ruta,"/");
+    strcat(ruta,equipo);
+    strcat(ruta,"/");
+    strcat(ruta,lista);
+    strcat(ruta,".json");
+    
+    if(remove(ruta)==0){
+        printf("La ruta se ha eliminado");
+    }else{
+        printf("Algo ha salido mal");
+    }
+    
+}
+void crearArchivo(char *bbdd, char *equipo){
+    
+    char ruta[200];
+    
+    strcpy(ruta,"db/");
+    strcat(ruta,bbdd);
+    strcat(ruta,"/");
+    strcat(ruta,equipo);
+    
+    if(mkdir(ruta,0777)==0){
+        printf("correcto");
+    }else{
+        printf("algo ha salido mal");
+    } 
+    
+}
+int main (int argc, char *argv[]){
+        char *opcion = argv[1];
+    
+        if (strcmp(opcion,"crear_archivo")==0){
+            crearArchivo(argv[2],argv[3]);
+        }else if(strcmp(opcion,"insertar")==0){
+            insertar(argv[2],argv[3],argv[4],argv[5]);
+        }else if(strcmp(opcion,"buscar")==0){
+            buscar(argv[2],argv[3],argv[4]);
+        }else if(strcmp(opcion,"eliminar")==0){
+            eliminar(argv[2],argv[3],argv[4]);
+        }else{
+            printf("No ha elegido nada");
+        }
+    
+    
+    return 0;
+}
